d3721799cfd8606c994c97bebbe2f4a58e81636e
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2023-10-29 16:20:44 -0400

    tests

--- test/Multisig.t.sol
+++ test/Multisig.t.sol
@@ -10,11 +10,15 @@ contract TestMultisig is Test {
 
     address internal alice;
     address internal bob;
+    address internal charlie;
+
     address[] internal owners;
 
     function setUp() public {
         alice = address(1);
         bob = address(2);
+        charlie = address(3);
+
         owners = [alice, bob];
 
         wallet = new Multisig(owners, 2);
@@ -25,10 +29,40 @@ contract TestMultisig is Test {
         assertEq(address(wallet).balance, 1 ether);
     }
 
+    function testPermissions() public {
+        vm.prank(alice);
+        uint256 transactionId = wallet.submitTxn(bob, 0.1 ether, "");
+
+        vm.expectRevert();
+        vm.prank(charlie);
+        wallet.approveTxn(transactionId);
+
+        vm.prank(bob);
+        wallet.approveTxn(transactionId);
+    }
+
+    function testCannotApproveMultipleTimes() public {
+        vm.prank(alice);
+        uint256 transactionId = wallet.submitTxn(bob, 0.1 ether, "");
+
+        vm.prank(alice);
+        wallet.approveTxn(transactionId);
+
+        vm.expectRevert();
+        vm.prank(alice);
+        wallet.executeTxn(transactionId);
+
+    }
+
     function testTransfer() public {
         vm.prank(alice);
         uint256 transactionId = wallet.submitTxn(bob, 0.1 ether, "");
         assertEq(alice.balance, 0 ether);
+
+        // not enough approvals yet
+        vm.expectRevert();
+        vm.prank(alice);
+        wallet.executeTxn(transactionId);
         
         vm.prank(bob);
         wallet.approveTxn(transactionId);
